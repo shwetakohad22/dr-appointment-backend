26120afca1474c313c63aa3660a38619fe59cc4a
shwetakohad22 <shwetakohad22@gmail.com>
2024-05-05 13:51:12 +0530

    changes done in server.js

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const AdminRouter = require("./routes/AdminRoute");
 dotenv.config();
 const cors = require("cors");
 const doctorRouter = require("./routes/DoctorRoute");
-const path = require("path");
 
 app.use(express.json());
 
@@ -19,12 +18,6 @@ app.use("/api/admin", AdminRouter);
 app.use("/api/doctor", doctorRouter);
 const port = process.env.PORT || 8001;
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/".express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
 app.listen(port, () => {
   console.log(`server is running on PORT ${port}`);
 });
